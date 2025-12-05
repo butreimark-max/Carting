9cab1e390ea43afec9f6d8576c3f8dd598a58448
noname <butreimark@gmail.com>
2025-12-05 17:00:10 -0500

    -

--- Carting code.py
+++ Carting code.py
@@ -1,67 +1,100 @@
 import arcade
+from pyglet.math import Vec2
 
 SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 600
 SCREEN_TITLE = ""
 
-class Car (arcade.Sprite):
+
+class Car(arcade.Sprite):
+    start = False
+    revers = False
+    stop = False
+
     def movement(self):
-        pass
-class truck (arcade.Sprite):
-    def movement_truck(self):
         self.center_x += self.change_x
         self.center_y += self.change_y
+
+        if self.start :  # todo: разобрать (так не пишем)
+            self.change_y += 0.3
+        if self.revers :
+            self.change_y -= 0.1
+        if self.stop :
+            if self.change_y>0:
+                self.change_y -=0.5
+            else:
+                self.change_y=0
 
 
 class MyGame(arcade.Window):
     def __init__(self, width, height, title):
         super().__init__(width, height, title)
-        """-----------------BackGround---------"""
-        self.race_road =truck ("pixil-frame-0 (6).png")
-        self.race_road.center_x = SCREEN_WIDTH
-        self.race_road.center_y = SCREEN_HEIGHT / 1.5
-        self.race_road.scale = 4
+        self.background = arcade.load_texture("pixil-frame-0 (6).png")
+        """--------------------Camera---------------"""
+        self.camera = arcade.Camera(SCREEN_WIDTH, SCREEN_HEIGHT)
         """------------------Car----------------------"""
-        self.first_car =Car ("pixil-frame-0 (4).png")
-        self.first_car.center_x = SCREEN_WIDTH /2
-        self.first_car.center_y = SCREEN_HEIGHT / 2
-        self.first_car.angle=0
+        self.first_car = Car("pixil-frame-0 (4).png")
+
+        self.first_car.center_x = self.background.width // 2
+        self.first_car.center_y = self.background.height // 2
+
+        self.first_car.angle = 0
+
     def on_key_press(self, symbol: int, modifiers: int):
-        if symbol==arcade.key.D:
-            self.race_road.angle=20
-            self.race_road.change_x = -10
-        if symbol==arcade.key.A:
-            self.race_road.angle=-20
-            self.race_road.change_x=10
-        if symbol==arcade.key.S:
-            self.race_road.change_y= 10
-        if symbol==arcade.key.W:
-            self.race_road.change_y=-10
+        if symbol == arcade.key.D:
+            self.first_car.angle = -20
+            self.first_car.change_x = 10
+        if symbol == arcade.key.A:
+            self.first_car.angle = 20
+            self.first_car.change_x = -10
+        if symbol == arcade.key.W:
+            self.first_car.start = True
+        if symbol == arcade.key.S:
+            self.first_car.revers = True
+        if symbol == arcade.key.SPACE:
+            self.first_car.stop = True
 
     def on_key_release(self, symbol: int, modifiers: int):
-        if symbol==arcade.key.A:
-            self.race_road.angle=0
-            self.race_road.change_x = 0
-        if symbol==arcade.key.D:
-            self.race_road.angle=0
-            self.race_road.change_x=0
-        if symbol==arcade.key.W:
-            self.race_road.change_y= 0
-        if symbol==arcade.key.S:
-            self.race_road.change_y=0
+        if symbol == arcade.key.A:
+            self.first_car.angle = 0
+            self.first_car.change_x = 0
+        if symbol == arcade.key.D:
+            self.first_car.angle = 0
+            self.first_car.change_x = 0
+        if symbol == arcade.key.S:
+            self.first_car.revers = False
+        if symbol == arcade.key.W:
+            self.first_car.start = False
+        if symbol == arcade.key.SPACE:
+            self.first_car.stop=False
+
     def on_draw(self):
         self.clear()
-        self.race_road.draw()
+        self.camera.use()
+        arcade.draw_lrwh_rectangle_textured(
+            0, 0,
+            self.background.width,
+            self.background.height,
+            self.background
+        )
         self.first_car.draw()
 
     def on_update(self, delta_time):
         self.first_car.movement()
-        self.race_road.movement_truck()
+
+        camera_x = self.first_car.center_x - SCREEN_WIDTH / 2
+        camera_y = self.first_car.center_y - SCREEN_HEIGHT / 2
+
+        self.camera.move_to(Vec2(camera_x, camera_y))
+
+        self.camera.angle = -self.first_car.angle
+
+
 
 window = MyGame(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
 arcade.run()
 
-#https://api.arcade.academy/en/2.6.17/examples/index.html
+# https://api.arcade.academy/en/2.6.17/examples/index.html
 """
 Найти все спрайты для проекта
 Чекнуть остальные примеры проектов от аркейда
